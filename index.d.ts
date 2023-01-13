1ef0df742e8456df6045a74ead454a7a3111f8f8
kleydon <kleydon@users.noreply.github.com>
2023-01-13 09:52:33 -0800

    Clean up comments

--- index.d.ts
+++ index.d.ts
@@ -1,19 +1,5 @@
-// Module d.ts file; enables this module to be used
+// Module d.ts file; enables using this module
 // in typescript react-native projects
-
-// If keeping the interface below up-to-date proves
-// to be a nuissance, options are:
-//
-// * Comment out everything below except the "declare module..." line
-//   (which enables this module to be used in typescript
-//   react-native projects). Cons: You lose auto-complete and 
-//   type-checking in VS Code, but, if the types defined by 
-//   the interface below aren't there - they can't be *wrong*.
-// 
-// * Re-create this module using create-react-native-library,
-//   choosing typescript, and java/obj-c. See:
-//   https://reactnative.dev/docs/native-modules-setup,
-//  
 
 // See:
 // https://www.typescriptlang.org/docs/handbook/declaration-files/templates/module-d-ts.html
