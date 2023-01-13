04394c37885c54e2af33ab62f294c9181bc8fb17
kleydon <kleydon@users.noreply.github.com>
2023-01-13 08:54:27 -0800

    update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,1 +1,15 @@
+//import Barometer from "."
+
 declare module "react-native-barometer";
+
+interface IBarometer {
+  watch: (watchCallbackFn:Function) => number,
+  clearWatch: (watchID:number) => void,
+  stopObserving: () => void,
+  isSupported: () => Promise<boolean>,
+  setInterval: (interval:number) => void,
+  setLocalPressure: (pressure:number) => void,
+}
+
+declare const Barometer:IBarometer;
+export default Barometer;
