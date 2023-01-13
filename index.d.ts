8ab4bbe70e2f7d5a8039eb4948678da06f56eb0a
kleydon <kleydon@users.noreply.github.com>
2023-01-13 12:45:28 -0800

    Update .d.ts file

--- index.d.ts
+++ index.d.ts
@@ -1,47 +1,19 @@
-// Module d.ts file; enables using this module
-// in typescript react-native projects
+// Module d.ts file
+
+// Enables using module in typescript react-native projects,
+// with editor auto-complete and type-validation (in VS Code)
 
 // See:
-// https://www.typescriptlang.org/docs/handbook/declaration-files/templates/module-d-ts.html
+// Typescript docs:
+//   https://www.typescriptlang.org/docs/handbook/declaration-files/templates/module-d-ts.html
+// Typescript Playground, for automatic generation of .d.ts file from .js:
+//   https://www.typescriptlang.org/play?filetype=js&useJavaScript=true#code/Q
+// Simplest approach:
 // https://stackoverflow.com/a/51355583
-
-// declare module "react-native-barometer";
-
-// import Barometer from ".";
-
-// interface BarometerPayload {  // See README for field descriptions
-//   timestamp:number,
-//   pressure:number,
-//   altitudeASL:number,
-//   altitude:number,
-//   relativeAltitude:number,
-//   verticalSpeed:number,
-// };
-
-// type WatchCallbackFn = (payload:BarometerPayload) => void;
-
-// interface IBarometer {
-//   watch: (watchCallbackFn:WatchCallbackFn) => number,
-//   clearWatch: (watchID:number) => void,
-//   stopObserving: () => void,
-//   isSupported: () => Promise<boolean>,
-//   setInterval: (interval:number) => void,
-//   setLocalPressure: (pressure:number) => void,
-// };
-
-// declare const Barometer:IBarometer;
-
-// export {
-//   Barometer,
-//   BarometerPayload,
-//   WatchCallbackFn
-// };
-
-// export default Barometer;
 
 declare module "react-native-barometer";
 
-interface BarometerPayload {  // See README for field descriptions
+interface BarometerPayload {  // See module README for field descriptions
   timestamp:number,
   pressure:number,
   altitudeASL:number,
@@ -52,12 +24,18 @@ interface BarometerPayload {  // See README for field descriptions
 
 type BarometerWatchCallbackFn = (payload:BarometerPayload) => void;
 
-export default Barometer;
-declare namespace Barometer {
-    function watch(success: BarometerWatchCallbackFn): number;
-    function clearWatch(watchID: any): void;
-    function stopObserving(): void;
-    function isSupported(): Promise<any>;
-    function setInterval(interval: any): void;
-    function setLocalPressure(pressure: any): void;
+declare namespace Barometer {  // See module README for function descriptions
+  function isSupported(): Promise<boolean>;
+  function setInterval(interval: number): void;
+  function setLocalPressure(pressure: number): void;
+  function watch(success: BarometerWatchCallbackFn): number;
+  function clearWatch(watchID: any): void;
+  function stopObserving(): void;
 }
+
+export {
+  BarometerPayload,
+  BarometerWatchCallbackFn
+};
+
+export default Barometer;
